a4004af683b4aefc01c91fb05677597d191bf0b3
squareclouds <sd@squareclouds.de>
2023-04-20 19:32:58 +0200

    WS usertables hidden, optimized focus-visible for everything

--- www/site/snippets/factories/loadmore.php
+++ www/site/snippets/factories/loadmore.php
@@ -1,12 +1,5 @@
-<li class="column" style="--columns: 3">
-    <a href="<?= $project->id() ?>">
-      <figure>
-        <span class="img" style="--w:4;--h:5">
-          <?= ($cover = $project->cover()) ? $cover->crop(400, 500) : null ?>
-        </span>
-        <figcaption class="img-caption">
-          <?= $project->title()->html() ?>
-        </figcaption>
-      </figure>
-    </a>
+<li class="column">
+  <a href="<?= $project->id() ?>">
+    <?= $project->title()->html() ?>
+  </a>
 </li>
